1275bd3817be0e8aa61eafa5362498e99578b51c
Raman_Kalita <raman_kalita@epam.com>
2017-03-31 09:48:44 +0300

    is is made it some better

--- components/App/App.js
+++ components/App/App.js
@@ -6,18 +6,18 @@ import TodoList from '../TodoList/TodoList.js'
 import AddTodoItem from '../AddTodoItem/AddTodoItem.js'
 import Spinner from '../Spinner/Spinner.js'
 
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <AddTodoItem />
+        <Spinner />
+        <TodoList />
+      </div>
+    );
+  }
+}
+
 export default connect(
   s => { return {} }
-)(
-  class App extends Component {
-    render() {
-      return (
-        <div className="app">
-          <AddTodoItem />
-          <Spinner />
-          <TodoList />
-        </div>
-      );
-    }
-  }
-  )
+)(App)
